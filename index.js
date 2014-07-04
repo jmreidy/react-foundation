2fc20a78c826b43e0f60206fb006744a32fa785d
Justin Reidy <jmreidy@rzrsharp.net>
2014-07-04 11:33:12 -0700

    adding components

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 var Foundation = {
   Button: require('./lib/Button'),
   Column: require('./lib/Column'),
+
+  // Form: require('./lib/Form'),
+  // FormGroup: require('./lib/FormGroup'),
+
+  // Input: require('./lib/Input'),
+
   Row: require('./lib/Row')
 };
 
 module.exports = Foundation;
+
